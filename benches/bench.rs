6c5ca130e99decdcb8370f7b1dee3e9f1cb80cb3
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-05-07 16:28:56 +0000

    Initial commit

---
+++ benches/bench.rs
@@ -0,0 +1,83 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use dispatch_bench::Dispatcher;
+use rand::Rng;
+use std::sync::atomic::{AtomicPtr, Ordering};
+
+#[inline(always)]
+fn xor(i: &[u8]) -> u8 {
+    use std::ops::BitXor;
+    i.iter().fold(0, u8::bitxor)
+}
+
+fn fancy_branching_xor(i: &[u8]) -> u8 {
+    Dispatcher::<false>::dispatch(|| xor(i))
+}
+
+fn fancy_indirect_xor(i: &[u8]) -> u8 {
+    Dispatcher::<true>::dispatch(|| xor(i))
+}
+
+fn standard_indirect_xor(i: &[u8]) -> u8 {
+    #[target_feature(enable = "avx2")]
+    #[target_feature(enable = "avx")]
+    unsafe fn avx2_avx(i: &[u8]) -> u8 {
+        xor(i)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn avx(i: &[u8]) -> u8 {
+        xor(i)
+    }
+
+    unsafe fn none(i: &[u8]) -> u8 {
+        xor(i)
+    }
+
+    static FN: AtomicPtr<()> = AtomicPtr::new(detect as *mut ());
+
+    fn detect(i: &[u8]) -> u8 {
+        let f = if is_x86_feature_detected!("avx") && is_x86_feature_detected!("avx") {
+            avx2_avx
+        } else if is_x86_feature_detected!("avx") {
+            avx
+        } else {
+            none
+        };
+
+        FN.store(f as *mut (), Ordering::Relaxed);
+
+        unsafe { f(i) }
+    }
+
+    unsafe {
+        let f = FN.load(Ordering::Relaxed);
+        let f: fn(&[u8]) -> u8 = std::mem::transmute(f);
+        f(i)
+    }
+}
+
+fn bench_dispatcher(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Dispatchers");
+    let mut rng = rand::thread_rng();
+    for len in [8, 1024].iter() {
+        let i: Vec<u8> = (0..*len).map(|_| rng.gen()).collect();
+        group.bench_with_input(
+            BenchmarkId::new("Fancy branching dispatch", len),
+            i.as_slice(),
+            |b, i| b.iter(|| fancy_branching_xor(i)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Fancy indirect dispatch", len),
+            i.as_slice(),
+            |b, i| b.iter(|| fancy_indirect_xor(i)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Standard indirect dispatch", len),
+            i.as_slice(),
+            |b, i| b.iter(|| standard_indirect_xor(i)),
+        );
+    }
+}
+
+criterion_group!(benches, bench_dispatcher);
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+pub struct Dispatcher<const INDIRECT: bool>;
+
+impl<const INDIRECT: bool> Dispatcher<INDIRECT> {
+    #[target_feature(enable = "avx2")]
+    #[target_feature(enable = "avx")]
+    unsafe fn avx2_avx<Output>(f: impl FnOnce() -> Output) -> Output {
+        f()
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn avx<Output>(f: impl FnOnce() -> Output) -> Output {
+        f()
+    }
+
+    unsafe fn none<Output>(f: impl FnOnce() -> Output) -> Output {
+        f()
+    }
+
+    pub fn dispatch<Output>(f: impl FnOnce() -> Output) -> Output {
+        static SELECTED: AtomicUsize = AtomicUsize::new(usize::MAX);
+        let selected = SELECTED.load(Ordering::Relaxed);
+        let selected = if selected == usize::MAX {
+            let selected = if is_x86_feature_detected!("avx2") & is_x86_feature_detected!("avx") {
+                2
+            } else if is_x86_feature_detected!("avx") {
+                1
+            } else {
+                0
+            };
+            SELECTED.store(selected, Ordering::Relaxed);
+            selected
+        } else {
+            selected
+        };
+
+        if INDIRECT {
+            let fns = [Self::none, Self::avx, Self::avx2_avx];
+            unsafe { fns.get_unchecked(selected)(f) }
+        } else {
+            match selected {
+                2 => unsafe { Self::avx2_avx(f) },
+                1 => unsafe { Self::avx(f) },
+                0 => unsafe { Self::none(f) },
+                _ => unsafe { std::hint::unreachable_unchecked() },
+            }
+        }
+    }
+}
